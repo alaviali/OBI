483427f752822eedbe886b61ac2c60b7e840b259
Meng Lei <raymon.ph@foxmail.com>
2015-03-01 22:35:51 -0800

    Dust sensor code

--- Meng/dust.ino
+++ Meng/dust.ino
@@ -4,35 +4,43 @@
 	Meng Lei, lmeng@pdx.edu
 	
 	Sensors: 	Groove Dust Sensor: PPD42NS
-				Sharp Dust Sensor: GP2Y1010AU0F
+	
+	http://www.seeedstudio.com/depot/grove-dust-sensor-p-1050.html
+	http://www.sca-shinyei.com/pdf/PPD42NS.pdf
+	
+	JST Pin 1 (Black Wire)  => Arduino GND
+	JST Pin 3 (Red wire)    => Arduino 5VDC
+	JST Pin 4 (Yellow wire) => Arduino Digital Pin 8
 */
 
-// ID pins
-#define GrovePin 7	// digital pin for grove dust sensor
-#define SharLedPin 6 // digital pin for controlling internal LED on Sharp dust sensor
+int pin = 8;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 1000;//sample 1s ;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
 
-// Setup variables
-//	// Grove dust variables
-float GroveConc = 999;	// in pcs/0.01cf (of particles >1um)
-float GroveRatio = 0;	// Initialize calculated ratio
-unsigned long GroveLowCount = 0;	// Number of samples that are low in interval
-unsigned long GroveHightCount = 0;	// Number of samples that are high in interval
-
-//	// Sharp dust variables
-int SharpVal = 999;	// Analog in from sharp dust sensor (0-5V)
-
-// Initialization
-void setup()
-{
-	Serial.begin(9600);	// Serial port speed; could be 115200
-	
-	pinMode(GrovePin, INPUT);
-	pinMode(SharLedPin, OUTPUT);
-	
-	analogReference(DEFAULT);
+void setup() {
+  Serial.begin(9600);
+  pinMode(8,INPUT);
+  starttime = millis();//get the current time;
 }
 
-void main()
-{
-	
+void loop() {
+  duration = pulseIn(pin, LOW);
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+
+  if ((millis()-starttime) > sampletime_ms)//if the sampel time == 1s
+  {
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    Serial.print(lowpulseoccupancy);
+    Serial.print(",");
+    Serial.print(ratio);
+    Serial.print(",");
+    Serial.println(concentration);
+    lowpulseoccupancy = 0;
+    starttime = millis();
+  }
 }

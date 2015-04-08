fd25e74de507798eb10a0f6cad45712cd5486f95
Meng Lei <raymon.ph@foxmail.com>
2015-04-08 15:04:31 -0700

    DHT22 rewritten, interrupt only, 4s interval
    See worklog on Google Drive for more details

---
+++ Individual sensor rewritten/DHT22.ino
@@ -0,0 +1,104 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+#include "DHT.h"
+
+#define DHTPIN 2     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11 
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht(DHTPIN, DHTTYPE);
+// NOTE: For working with a faster chip, like an Arduino Due or Teensy, you
+// might need to increase the threshold for cycle counts considered a 1 or 0.
+// You can do this by passing a 3rd parameter for this threshold.  It's a bit
+// of fiddling to find the right value, but in general the faster the CPU the
+// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+// Arduino Due that runs at 84mhz a value of 30 works.
+// Example to initialize DHT sensor for Arduino Due:
+//DHT dht(DHTPIN, DHTTYPE, 30);
+
+
+//toggle variables to monitor timer interrupt status using oscilloscope
+boolean toggle1 = 0;
+
+void setup() {
+	Serial.begin(9600);
+	Serial.println("DHTxx test!");
+
+	dht.begin();
+
+	cli();//stop interrupts
+
+	//set timer1 interrupt at 1Hz
+	TCCR1A = 0;// set entire TCCR1A register to 0
+	TCCR1B = 0;// same for TCCR1B
+	TCNT1 = 0;//initialize counter value to 0
+	// set compare match register for 1hz increments
+	OCR1A = 62499;// must be <65536, see worklog for calculation
+	// turn on CTC mode
+	TCCR1B |= (1 << WGM12);
+	// Set CS12 and CS10 bits for 1024 prescaler
+	TCCR1B |= (1 << CS12) | (1 << CS10);
+	// enable timer compare interrupt
+	TIMSK1 |= (1 << OCIE1A);
+
+	sei();//enable global interrupts
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	//generates pulse wave of the same fequency as the interrupt, monitor using oscilloscope
+	if (toggle1){
+		digitalWrite(13, HIGH);
+		toggle1 = 0;
+	}
+	else{
+		digitalWrite(13, LOW);
+		toggle1 = 1;
+	} // THIS PART NEEDS TO BE DELETED IN FINAL CODE!
+
+	// Reading temperature or humidity takes about 250 milliseconds!
+	// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+	float h = dht.readHumidity();
+	// Read temperature as Celsius
+	float t = dht.readTemperature();
+	// Read temperature as Fahrenheit
+	float f = dht.readTemperature(true);
+
+	// Check if any reads failed and exit early (to try again).
+	if (isnan(h) || isnan(t) || isnan(f)) {
+		Serial.println("Failed to read from DHT sensor!");
+		return;
+	}
+
+	// Compute heat index
+	// Must send in temp in Fahrenheit!
+	float hi = dht.computeHeatIndex(f, h);
+
+	Serial.print("Humidity: ");
+	Serial.print(h);
+	Serial.print(" %\t");
+	Serial.print("Temperature: ");
+	Serial.print(t);
+	Serial.print(" *C ");
+	Serial.print(f);
+	Serial.println(" *F\t");
+}
+
+void loop() {
+	// Wait a few seconds between measurements.
+	delay(500);
+	__asm__("nop");
+}
+

f08842bce4f59ab941bef5cb521dfb5ca14703ee
Meng Lei <raymon.ph@foxmail.com>
2015-04-12 20:37:33 -0700

    2nd merge without pulse sensor

--- Merged/Merged.ino
+++ Merged/Merged.ino
@@ -32,7 +32,6 @@ float offTime = 480;
 unsigned long startMillis;
 unsigned long switchTimeMillis;
 boolean heaterInHighPhase;
-#include "MQ7.cpp"
 
 // **************** FOR TEMP & HUMIDITY SENSOR ****************
 
@@ -177,3 +176,39 @@ void loop()
 	// Wait a few seconds between measurements.
 	delay(500);
 }
+
+
+// **************** FOR MQ7 SENSOR ****************
+void turnHeaterHigh(){
+	// 5v phase
+	digitalWrite(VOLTAGE_REGULATOR_DIGITAL_OUT_PIN, LOW);
+	heaterInHighPhase = true;
+	switchTimeMillis = millis() + MQ7_HEATER_5_V_TIME_MILLIS;
+}
+
+void turnHeaterLow(){
+	// 1.4v phase
+	digitalWrite(VOLTAGE_REGULATOR_DIGITAL_OUT_PIN, HIGH);
+	heaterInHighPhase = false;
+	switchTimeMillis = millis() + MQ7_HEATER_1_4_V_TIME_MILLIS;
+}
+
+void readGasLevel(){
+	unsigned int gasLevel = analogRead(MQ7_ANALOG_IN_PIN);
+	unsigned int time = (millis() - startMillis) / 1000;
+
+	if (heaterInHighPhase)
+	{
+		Serial.print(time);
+		Serial.print(",");
+		Serial.println("-");
+	}
+	else
+	{
+		Serial.print(time);
+		Serial.print(",");
+		Serial.println(gasLevel);
+	}
+
+}
+

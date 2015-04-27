fd7881aec3658df6d2cfa08dd175d3c025197805
Meng Lei <raymon.ph@foxmail.com>
2015-04-26 19:25:09 -0700

    Memory code updated
    Problem is in hardware

--- Meng/Memory.ino
+++ Meng/Memory.ino
@@ -41,20 +41,24 @@ Used the following components and wire routing:
 uint8_t Spi23LC1024Read8(uint32_t address) {
 	uint8_t read_byte;
 
+	PORTB &= ~(1 << PORTB2);        //set SPI_SS low
 	SPI.transfer(READ);
 	SPI.transfer((uint8_t)(address >> 16) & 0xff);
 	SPI.transfer((uint8_t)(address >> 8) & 0xff);
 	SPI.transfer((uint8_t)address);
 	read_byte = SPI.transfer(0x00);
+	PORTB |= (1 << PORTB2);         //set SPI_SS high
 	return read_byte;
 }
 
 void Spi23LC1024Write8(uint32_t address, uint8_t data_byte) {
+	PORTB &= ~(1 << PORTB2);        //set SPI_SS low
 	SPI.transfer(WRITE);
 	SPI.transfer((uint8_t)(address >> 16) & 0xff);
 	SPI.transfer((uint8_t)(address >> 8) & 0xff);
 	SPI.transfer((uint8_t)address);
 	SPI.transfer(data_byte);
+	PORTB |= (1 << PORTB2);         //set SPI_SS high
 }
 
 void setup(void) {
@@ -66,8 +70,8 @@ void setup(void) {
 
 	for (i = 0; i<32; i++) {
 		Spi23LC1024Write8(i, (uint8_t)i);
-		value =  (i);
-		Serial.println((uint16_t)value);
+		value = Spi23LC1024Read8(i);
+		Serial.println(value);
 	}
 }
 
